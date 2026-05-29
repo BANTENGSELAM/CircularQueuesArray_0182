34b71f5c4f6f02c58af794889c6ea4580ca1b647
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-05-29 14:53:29 +0700

    cek jika antrian hanya memiliki satu elemen

--- circularqa.cpp
+++ circularqa.cpp
@@ -51,6 +51,17 @@ class Queues {
                     cout << "queue underflow\n";
                     return;
             }
+            cout << "\n The element deleted from the queue is : " << queue_array[FRONT] << "\n";
+
+            // cek jika antrian hanya memiliki satu elemen
+            if (FRONT == REAR)
+            {
+                    FRONT = -1;
+                    REAR = -1;
+            }
+            else{ 
+
+            }
         }
 };
 

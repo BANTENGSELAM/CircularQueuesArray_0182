4c5ff102293d77c3b8b992ddfac5e65032a35b0f
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-05-29 15:01:13 +0700

    iterasi dari awal array hingga REAR

--- circularqa.cpp
+++ circularqa.cpp
@@ -102,7 +102,14 @@ class Queues {
                }
 
                FRONT_Position = 0;
-               
+
+               //iterasi dari awal array hingga REAR
+               while (FRONT_Position <= REAR_Position)
+               {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+               }
+               cout << endl;
             }
         }
 };
